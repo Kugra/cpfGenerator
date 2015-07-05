0039427e0cd6fcf6c1cf70f54434227bb88a3141
Kugra <johan.kovalsikoski@gmail.com>
2015-07-05 17:46:22 -0300

    Adicionando linha para script

--- cpfGenerator.py
+++ cpfGenerator.py
@@ -1,22 +1,20 @@
-__author__ = 'aluno01'
+#!/usr/bin/env python3
+#-*-coding: utf-8 -*-
 
-"""
-Não criei funções por que não quis :v
-Codigo feito nas coxas,pode melhorar .-.
-"""
+__author__ = 'Johan'
 
 import random
 
 gerados = []
-
 somaD1 = 0
 somaD2 = 0
+
+
 for fCtrl in range(9):
     randomizado = random.randint(0, 9)
     gerados.append(randomizado)
 
 ctrlMult=10
-
 for mCtrl in range(9):
     somaD1+=(gerados[mCtrl]*ctrlMult)
     ctrlMult=ctrlMult-1
@@ -27,10 +25,7 @@ if (somaD1-ajuste*11 <2):
 else:
     gerados.append(11-(somaD1-ajuste*11))
 
-#aqui
-
 ctrlMultI2 = 11
-
 for mCtrlI2 in range(10):
     somaD2+=gerados[mCtrlI2]*ctrlMultI2
     ctrlMultI2=ctrlMultI2-1
